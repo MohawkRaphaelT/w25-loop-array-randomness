5e586e0ea5f6bbd5efed6783fff3b9102e16e8f9
MohawkRaphaelT <150467892+MohawkRaphaelT@users.noreply.github.com>
2025-01-28 10:13:12 -0500

    Add base drawing

--- tetreault-raph-a2-cool-sky-drawing/Game.cs
+++ tetreault-raph-a2-cool-sky-drawing/Game.cs
@@ -3,31 +3,79 @@ using System;
 using System.Numerics;
 
 // The namespace your code is in.
-namespace MohawkGame2D
+namespace MohawkGame2D;
+
+/// <summary>
+///     Your game code goes inside this class!
+/// </summary>
+public class Game
 {
+    // Place your variables here:
+    Color deepBlue = new Color("051f39");
+    Color purple = new Color("4a2480");
+    Color magenta = new Color("c53a9d");
+    Color softOrange = new Color("ff8e80");
+    int starCount = 33;
+    int[] starPositionsX;
+    int[] starPositionsY;
+
+
     /// <summary>
-    ///     Your game code goes inside this class!
+    ///     Setup runs once before the game loop begins.
     /// </summary>
-    public class Game
+    public void Setup()
     {
-        // Place your variables here:
+        Window.SetTitle("Cool Sky Drawing");
+        Window.SetSize(400, 400);
+        Draw.LineSize = 0;
 
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
-        public void Setup()
+        // Generate random star positions
+        // First, initialize the array to contain a certain number of elements
+        starPositionsX = new int[starCount];
+        starPositionsY = new int[starCount];
+        // Next, go through array and assign each index a value
+        for (int i = 0; i < starCount; i++)
         {
-
-        }
-
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
-        public void Update()
-        {
-
+            starPositionsX[i] = Random.Integer(0, 400); // x = 0-400
+            starPositionsY[i] = Random.Integer(0, 300); // y = 0-300
         }
     }
 
+    /// <summary>
+    ///     Update runs every frame.
+    /// </summary>
+    public void Update()
+    {
+        // Draw sky
+        Window.ClearBackground(deepBlue);
+
+        // Draw stars
+        Draw.FillColor = softOrange;
+        for (int i = 0; i < starCount; i++)
+        {
+            Draw.Circle(starPositionsX[i], starPositionsY[i], 2);
+        }
+
+        // Draw background clouds
+        Draw.FillColor = purple;
+        for (int i = 0; i < 16; i++)
+        {
+            int x = 12 + i * 25;
+            Draw.Circle(x, Window.Height - 100, 19);
+        }
+        // Draw midground clouds
+        Draw.FillColor = magenta;
+        for (int i = 0; i < 8; i++)
+        {
+            int x = 25 + i * 50;
+            Draw.Circle(x, Window.Height - 65, 38);
+        }
+        // Draw foreground clouds
+        Draw.FillColor = softOrange;
+        for (int i = 0; i < 4; i++)
+        {
+            int x = 50 + i * 100;
+            Draw.Circle(x, Window.Height, 75);
+        }
+    }
 }
